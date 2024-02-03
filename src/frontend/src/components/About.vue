d3cb30423376ca8a1f10b6ec880843978b6c15d8
colsi <simone.colecchia@mail.polimi.it>
2024-02-03 12:40:30 +0100

    About page

---
+++ src/frontend/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container" style="display: flex; flex-direction: column; justify-content: center; align-items: center">
+    <img src="@/assets/logoscritta.png" alt="CodeKataBattle" width="1500">
+
+    <div class="container m-5" style="display: flex; justify-content: center; align-items: end; flex-direction: column">
+      <h5 style="width: fit-content">by</h5>
+      <h3 style="width: fit-content; margin-top: 5%">Alessandro Fornara</h3>
+      <h3 style="width: fit-content">Simone Colecchia</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "About"
+}
+</script>
